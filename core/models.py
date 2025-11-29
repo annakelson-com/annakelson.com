cdfeaf8e9cbef82d62fc6bdacd3da184367abdb3
Hans K <hans@communitycomputing.net>
2025-11-28 22:14:42 -0500

    Implement the beginning of standard page styling

--- core/models.py
+++ core/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.blocks import RichTextBlock
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.fields import StreamField
 from wagtail.models import Page
 
 
@@ -6,5 +10,15 @@ from wagtail.models import Page
 
 
 class StandardPage(Page):
-    pass
+    body = StreamField(
+        [
+            ('rich_text', RichTextBlock()),
+            ('embed', EmbedBlock()),
+        ],
+        null=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
 

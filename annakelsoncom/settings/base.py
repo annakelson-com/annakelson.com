e47b40528af058563c79894b5802eec221ab6d3b
Hans Kelson <hans@communitycomputing.net>
2025-12-03 12:27:49 -0500

    Put garbage in the base URL to see what happens

--- annakelsoncom/settings/base.py
+++ annakelsoncom/settings/base.py
@@ -175,7 +175,7 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "https://annakelson.com"
+WAGTAILADMIN_BASE_URL = "https://annedrrthpikjrepjoakelson.com"
 
 # Allowed file extensions for documents in the document library.
 # This can be omitted to allow all files, but note that this may present a security risk
